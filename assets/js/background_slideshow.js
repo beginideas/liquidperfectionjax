3667db800cbf755943b03e34a832708fef03500b
Sonny Mattera <smattera@hotmail.com>
2023-01-23 18:47:59 -0600

    Update background_slideshow.js

--- assets/js/background_slideshow.js
+++ assets/js/background_slideshow.js
@@ -2,9 +2,9 @@ var i = 0;
 var images = [];
 var slideTime = 3000; // 3 seconds
 
-images[0] = 'http://www.choctawindianfair.com/liquidperfectionjax/assets/images/pic01.png';
-images[1] = 'http://www.choctawindianfair.com/liquidperfectionjax/assets/images/pic02.png';
-images[2] = 'http://www.choctawindianfair.com/liquidperfectionjax/assets/images/pic03.png';
+images[0] = 'http://www.choctawindianfair.com/liquidperfectionjax/assets/images/pic01.jpg';
+images[1] = 'http://www.choctawindianfair.com/liquidperfectionjax/assets/images/pic02.jpg';
+images[2] = 'http://www.choctawindianfair.com/liquidperfectionjax/assets/images/pic03.jpg';
 
 function changePicture() {
     document.body.style.backgroundImage = "url(" + images[i] + ")";
