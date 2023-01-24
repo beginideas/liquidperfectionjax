35aadcb554f229ef4df70ec07b52a287b761958f
Sonny Mattera <smattera@hotmail.com>
2023-01-23 19:27:32 -0600

    Update main.scss

--- assets/js/background_slideshow.js
+++ assets/js/background_slideshow.js
@@ -2,9 +2,11 @@ var i = 0;
 var images = [];
 var slideTime = 3000; // 3 seconds
 
-images[0] = 'http://www.choctawindianfair.com/liquidperfectionjax/assets/images/pic01.jpg';
-images[1] = 'http://www.choctawindianfair.com/liquidperfectionjax/assets/images/pic02.jpg';
-images[2] = 'http://www.choctawindianfair.com/liquidperfectionjax/assets/images/pic03.jpg';
+images[0] = '/assets/images/work/ash.webp';
+images[1] = '/assets/images/work/ash1.webp';
+images[2] = '/assets/images/work/ash2.webp';
+images[3] = '/assets/images/work/ash3-300x225.webp';
+images[4] = '/assets/images/work/ash4.webp';
 
 function changePicture() {
     document.getElementById("banner").style.backgroundImage = "url(" + images[i] + ")";
